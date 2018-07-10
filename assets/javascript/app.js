2a2ca1f7f98cdb2aa4896c2b8e35e488f8955c89
Andrew Prine <andrewpprine@gmail.com>
2018-07-10 11:03:03 -0700

    cleaning

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,14 +2,11 @@
 
 $('button').click(function (){
   var input = $('input').val();
-  var queryURL = `https://api.giphy.com/v1/gifs/search?api_key=s1ZtYEWgR2UGbcVY0dPXq32Q5gMlN9Xf&q=`+input+`&limit=10&offset=0&rating=PG&lang=en`
+  var queryURL = `https://api.giphy.com/v1/gifs/search?api_key=s1ZtYEWgR2UGbcVY0dPXq32Q5gMlN9Xf&q=`+input+`&limit=10&offset=0&rating=PG-13&lang=en`
   $.ajax({
     url: queryURL
   }).then(function(response) {
     console.log(response);
-    console.log(response.data[0]);
-    console.log(response.data[0].url);
-    console.log(response.data[0].rating);
     console.log(response.data[0].images.original.url);
     // var gifArt = response.data[0].url;
     var gifArt = $('<img>').attr('src',response.data[0].images.original.url);
