056844c1924edc298b38653ff1e45547b88dd508
Andrew Prine <andrewpprine@gmail.com>
2018-07-12 19:30:32 -0700

    auto buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,20 @@
 
 var foods = [`pizza`, `taco`, `sandwich`, `donut`,`ice cream`]
+var newImage;
+var newButton;
+var newDiv;
 
-$('button').click(function (){
+//pre-made search buttons
+function buttonMaker(){
+  newButton = $('<button>').text(foods[x]);
+  $('#buttonArea').append(newButton);
+  }
+  for(x in foods){
+    buttonMaker(foods[x]);
+  }
+
+
+$('#makeButton').click(function (){
   var input = $('input').val();
   var queryURL = `https://api.giphy.com/v1/gifs/search?api_key=s1ZtYEWgR2UGbcVY0dPXq32Q5gMlN9Xf&q=`+input+`&limit=10&offset=0&rating=PG-13&lang=en`
   $.ajax({
@@ -9,9 +22,7 @@ $('button').click(function (){
   }).then(function(response) {
     console.log(response);
     console.log(response.data[0].images.original.url);
-    // var gifArt = response.data[0].url;
     for(x=0;x<10;x++){
-
       var gifArt = $('<img>').attr('src',response.data[x].images.original.url);
       var gifRating = response.data[x].rating;
       $('#results').prepend(gifArt);
